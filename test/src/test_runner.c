99ffcfd91790149e077a8c776fc783fb23559eab
Juan Pablo Saucedo <saucedojuanpablo@gmail.com>
2021-04-05 14:49:07 -0300

    Makefile mas picante y added test suite vacía

---
+++ test/src/test_runner.c
@@ -0,0 +1,20 @@
+#include "../include/minunit.h"
+#include "test_oper.c"
+
+MU_TEST_SUITE(calc_test_suite)
+{
+    MU_RUN_TEST(test_sum_int);
+    MU_RUN_TEST(test_sum_bin);
+    MU_RUN_TEST(test_res_int);
+    MU_RUN_TEST(test_res_bin);
+}
+
+int main()
+{
+    MU_RUN_SUITE(calc_test_suite);
+    MU_REPORT();
+    
+    // Para que rebote el build jenkins, tiene que devolver 1
+    if (minunit_fail > 0) return 1; 
+    else return 0;
+}
