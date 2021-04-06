db7c26c86f4546fba46829e6cc7dd8e64c862b65
Juan Pablo Saucedo <saucedojuanpablo@gmail.com>
2021-04-06 16:58:07 -0300

    parsea la expresión y elige entre suma o resta

--- src/calc.c
+++ src/calc.c
@@ -4,11 +4,13 @@
 #include <stdlib.h>
 
 void PRE_CDECL calc_sum( int, int, int * ) POST_CDECL;
+void PRE_CDECL calc_resta( int, int, int * ) POST_CDECL;
 
 int main( int argc, char **argv )  {
    int opt;
    int bflag = 0; // Flag para binario
 
+   void (*operacion)(int, int, int*);
    int operando_1, operando_2, resultado;
    char operador;
 
@@ -18,9 +20,18 @@ int main( int argc, char **argv )  {
    
    printf("Ingrese la operación a realizar\n> ");
    scanf("%d %c %d", &operando_1, &operador, &operando_2);
-   printf("Usted ingresó: %d %c %d", operando_1, operador, operando_2);
-   calc_sum(operando_1, operando_2, &resultado);
+   printf("Usted ingresó: %d %c %d\n", operando_1, operador, operando_2);
+
+
+   if (operador == '+')
+      operacion = calc_sum;
+   else if (operador == '-')
+      operacion = calc_resta;
+   else 
+      return 1;
+   operacion(operando_1, operando_2, &resultado);
    printf("El resultado es: %x, en %x\n", resultado, (unsigned int)&resultado);
+   return 0;
 
 }
 
