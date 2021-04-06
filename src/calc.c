d8eb0a38a1a2bbb89f49fa7de31ee68d55c65773
Juan Pablo Saucedo <saucedojuanpablo@gmail.com>
2021-04-06 16:06:44 -0300

    toma opción de binario y parsea expresión

--- src/calc.c
+++ src/calc.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main( int argc, char *argv[] )  {
-/*
-   if( argc == 4 ) {
-      printf("The %d  arguments supplied are %s %s %s \n",argc, argv[1], argv[2],argv[3]);
-   }
-   else {
-      printf("Four arguments expected. <number><operand><number> \n");
-   
-   }
-   
-   for (int i = 0; i <= argc; i++) {
-    printf("%d at %p: %s\n", i, argv[i], argv[i]);
-  }
-   
-  */
-   int z = 0; 
+int main( int argc, char **argv )  {
+   int opt;
+   int bflag = 0; // Flag para binario
 
-   if ((char)argv[2][0]=='+')
-   {
-   int y = atoi(argv[1]);
-   int x = atoi(argv[3]);
-   z = x + y;
-   }
-   printf("%d", z);
+   int operando_1, operando_2;
+   char operador;
+
+   opt = getopt(argc, argv, "b");
+   if (opt == 'b')
+      bflag = 1;
+   
+   printf("Ingrese la operación a realizar\n> ");
+   scanf("%d %c %d", &operando_1, &operador, &operando_2);
+   printf("Usted ingresó: %d %c %d", operando_1, operador, operando_2);
 
   return 0;
 
